7d8cc304d40139a3eac313553b65a6740093f290
Jan Douwe Zelhorst <110466722+Jdzelhorst@users.noreply.github.com>
2023-11-13 15:55:41 +0000

    Added basic NavBar from the bootstrap directory

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World!
-        </p>
-      </header>
+      <NavBar />
     </div>
   );
 }
