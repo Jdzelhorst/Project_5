7e039793fbc11948b4cb544b1631e013b53d01cf
Jan Douwe Zelhorst <110466722+Jdzelhorst@users.noreply.github.com>
2023-11-13 21:20:07 +0000

    Added the initial sign-up form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import styles from './App.module.css'
 import NavBar from './components/NavBar';
 import Container from 'react-bootstrap/Container';
 import { Route, Routes } from 'react-router-dom';
+import './api/axiosDefaults';
+import SignUpForm from './pages/auth/SignUpForm';
+
 
 function App() {
   return (
@@ -11,7 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<h2>Home</h2>} />
           <Route path="sign-in/" element={<h2>Sing In</h2>} />
-          <Route path="sign-up/" element={<h2>Sing Up</h2>} />
+          <Route path="sign-up/" element={<SignUpForm />} />
           <Route path='*' element={<h2>Page not found!</h2>} />
         </Routes>
       </Container>
