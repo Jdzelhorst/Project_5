f62bc366b9a1d4afef447994b1692d8c1eac319f
Jan Douwe Zelhorst <110466722+Jdzelhorst@users.noreply.github.com>
2023-11-13 18:47:35 +0000

    Installed React-router latest version (6.4), added route paths to the corresponding navbar elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import styles from './App.module.css'
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<h2>Home</h2>} />
+          <Route path="sign-in/" element={<h2>Sing In</h2>} />
+          <Route path="sign-up/" element={<h2>Sing Up</h2>} />
+        </Routes>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,13 +8,13 @@ const NavBar = () => {
     return (
         <Navbar expand="lg" className={styles.Navbar} sticky="top">
           <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="ms-auto p-2">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#sign-in">Sign In</Nav.Link>
-                <Nav.Link href="#sign-up">Sign Up</Nav.Link>
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="sign-in">Sign In</Nav.Link>
+                <Nav.Link href="sign-up">Sign Up</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
